af22ba28895b8a081fe0a50d228217df0ca4872c
David <dasilvadav07@gmail.com>
2021-03-28 16:56:42 +0200

    parcel

--- David/dist/scriptP1.3eff2a5d.js
+++ David/dist/scriptP1.3eff2a5d.js
@@ -441,7 +441,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51331" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
