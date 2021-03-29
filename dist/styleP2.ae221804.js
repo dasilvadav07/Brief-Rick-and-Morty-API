81210a55077b5aa6aab6cc88cec50133241760f1
David <dasilvadav07@gmail.com>
2021-03-29 17:52:26 +0200

    parcel main

--- dist/styleP2.ae221804.js
+++ dist/styleP2.ae221804.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"David/styleP1.scss":[function(require,module,exports) {
+},{"./bundle-url":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"Caroline/styleP2.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styleP1.6e91f429.js.map
+//# sourceMappingURL=/styleP2.ae221804.js.map
